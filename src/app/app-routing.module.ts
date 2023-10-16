dc855184e0faa5ca6e10a259a38e0309c5aa88a1
metachoSam <samuel.f.met@gmail.com>
2023-10-16 17:30:52 -0500

    Home y agenda de citas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
-import {HomeComponent} from "./home/home.component";
+import {HomeComponent} from "./components/home/home.component";
 import {canActivate, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
+import {PersonalComponent} from "./pages/personal/personal.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path:'register', component: RegisterComponent  },
+  { path:'personal', component: PersonalComponent  },
   { path:  'home', component:HomeComponent,
     ...canActivate(()=> redirectUnauthorizedTo(['/login']))
   }
