f82d6062ed54eb15d691843b71c28c5ae88c94df
metachoSam <samuel.f.met@gmail.com>
2023-10-23 18:44:12 -0500

    LogOut

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Swal from "sweetalert2";
+import {Router} from "@angular/router";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {
+  }
+
+  logOut() {
+    Swal.fire({
+      title: '¿Quieres cerrar sesion?',
+      showDenyButton: true,
+      confirmButtonText: 'Salir',
+      denyButtonText: `No salir`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.userService.logout().then(r => {})
+        this.router.navigate(['/login']).then(r => {});
+      }
+    })
+  }
 
 }
